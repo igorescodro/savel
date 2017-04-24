3f81125f9dbe76257b07c93958954224b7fb17c6
Igor Escodro <escodro@outlook.com>
2017-04-24 15:55:13 -0300

    Update MusicBrainz query parameter
    Update from "artist" to "sortname" to seek better results

--- app/src/main/java/com/escodro/savel/data/remote/repository/MusicBrainzRepository.java
+++ app/src/main/java/com/escodro/savel/data/remote/repository/MusicBrainzRepository.java
@@ -49,7 +49,7 @@ public class MusicBrainzRepository {
      * @return observable of MusicBrainz Artist List entity
      */
     public Observable<MusicBrainzArtistList> searchArtist(String artistName) {
-        artistName = "artist:" + artistName;
+        artistName = "sortname:" + artistName;
         return mService.searchArtist(artistName);
     }
 }

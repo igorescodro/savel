2435b9b035e0e5d2cccf134c6d71720fc6932003
Igor Escodro <escodro@outlook.com>
2017-05-10 16:28:00 -0300

    Update Instagram POJO to follow LoD
    Instagram POJOs updated to follow Law of Demeter

--- app/src/main/java/com/escodro/savel/data/model/instagram/InstagramCaption.java
+++ app/src/main/java/com/escodro/savel/data/model/instagram/InstagramCaption.java
@@ -19,37 +19,4 @@ public class InstagramCaption {
 
     @SerializedName("from")
     private InstagramData from;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getCreatedTime() {
-        return createdTime;
-    }
-
-    public void setCreatedTime(String createdTime) {
-        this.createdTime = createdTime;
-    }
-
-    public InstagramData getFrom() {
-        return from;
-    }
-
-    public void setFrom(InstagramData from) {
-        this.from = from;
-    }
-
 }

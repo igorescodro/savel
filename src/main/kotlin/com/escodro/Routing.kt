fe6e19642903ac37009b35783a609a2613c97a6b
Igor Escodro <escodro@outlook.com>
2024-12-13 19:58:58 -0500

    🎉 Initial commit
    The adventure begins!

---
+++ src/main/kotlin/com/escodro/Routing.kt
@@ -0,0 +1,16 @@
+package com.escodro
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        get("/testing") {
+            call.respondText("This is a test!")
+        }
+    }
+}

94260e6136b868385f59662dc57f2a30b3574aa9
Igor Escodro <escodro@outlook.com>
2024-12-27 16:46:01 -0500

    💿 Implement more providers and refactor logic
    More providers were added and the code was updated to be more scalable
    as we grow.

--- src/main/kotlin/com/escodro/savel/core/model/provider/ExternalProvider.kt
+++ src/main/kotlin/com/escodro/savel/core/model/provider/ExternalProvider.kt
@@ -2,4 +2,15 @@ package com.escodro.savel.core.model.provider
 
 enum class ExternalProvider {
     Spotify,
+    AppleMusic,
+    Instagram,
+    Deezer,
+    Tidal,
+    Youtube,
+    YouTubeMusic,
+    AmazonMusic,
+    TikTok,
+    Facebook,
+    Threads,
+    Twitter,
 }
--- src/main/kotlin/com/escodro/savel/data/remote/musicbrainz/mapper/ExternalResourceMapper.kt
+++ src/main/kotlin/com/escodro/savel/data/remote/musicbrainz/mapper/ExternalResourceMapper.kt
@@ -6,26 +6,61 @@ import com.escodro.savel.data.remote.musicbrainz.model.artist.Relation
 
 internal class ExternalResourceMapper {
     fun toExternalResourceList(relationList: List<Relation>?): List<ExternalResource>? {
-        if (relationList == null) return null
-        return listOfNotNull(
-            getSpotifyExternalResource(relationList),
-        )
+        if (relationList == null) {
+            return null
+        }
+
+        return with(relationList) {
+            listOfNotNull(
+                getExternalResource(mask = SPOTIFY_MASK, provider = ExternalProvider.Spotify, idIndex = 4),
+                getExternalResource(mask = APPLE_MUSIC_MASK, provider = ExternalProvider.AppleMusic, idIndex = 5),
+                getExternalResource(mask = INSTAGRAM_MASK, provider = ExternalProvider.Instagram, idIndex = 3),
+                getExternalResource(mask = DEEZER_MASK, provider = ExternalProvider.Deezer, idIndex = 4),
+                getExternalResource(mask = TIDAL_MASK, provider = ExternalProvider.Tidal, idIndex = 4),
+                getExternalResource(mask = YOUTUBE_MASK, provider = ExternalProvider.Youtube, idIndex = 4),
+                getExternalResource(mask = YOUTUBE_MUSIC_MASK, provider = ExternalProvider.YouTubeMusic, idIndex = 4),
+                getExternalResource(mask = AMAZON_MUSIC_MASK, provider = ExternalProvider.AmazonMusic, idIndex = 4),
+                getExternalResource(mask = TIKTOK_MASK, provider = ExternalProvider.TikTok, idIndex = 3),
+                getExternalResource(mask = FACEBOOK_MASK, provider = ExternalProvider.Facebook, idIndex = 3),
+                getExternalResource(mask = THREADS_MASK, provider = ExternalProvider.Threads, idIndex = 3),
+                getExternalResource(mask = TWITTER_MASK, provider = ExternalProvider.Twitter, idIndex = 3),
+            ).flatten()
+        }
     }
 
-    fun toArtistImageId(relationList: List<Relation>): String? = getSpotifyExternalResource(relationList)?.id
+    fun toArtistImageId(relationList: List<Relation>): String? =
+        relationList
+            .getExternalResource(mask = SPOTIFY_MASK, provider = ExternalProvider.Spotify, idIndex = 4)
+            .firstOrNull()
+            ?.id
 
-    private fun getSpotifyExternalResource(relationList: List<Relation>): ExternalResource? {
-        val url = relationList.find { relation -> relation.url.resource.contains(SPOTIFY_MASK) }?.url?.resource
-        val id = url?.split("/")?.get(4) ?: return null
-
-        return ExternalResource(
-            provider = ExternalProvider.Spotify,
-            id = id,
-            url = url,
-        )
+    private fun List<Relation>.getExternalResource(
+        mask: String,
+        provider: ExternalProvider,
+        idIndex: Int,
+    ): List<ExternalResource> {
+        val providerList = this.filter { it.url.resource.contains(mask) }
+        return providerList.mapNotNull { relation ->
+            val id =
+                relation.url.resource
+                    .split("/")
+                    .getOrNull(idIndex) ?: return@mapNotNull null
+            ExternalResource(provider = provider, id = id, url = relation.url.resource)
+        }
     }
 
     private companion object {
-        const val SPOTIFY_MASK = "https://open.spotify.com/artist/"
+        const val SPOTIFY_MASK = "open.spotify.com/artist/"
+        const val APPLE_MUSIC_MASK = "music.apple.com/"
+        const val INSTAGRAM_MASK = "instagram.com/"
+        const val DEEZER_MASK = "deezer.com/"
+        const val TIDAL_MASK = "tidal.com/artist/"
+        const val YOUTUBE_MASK = "www.youtube.com/channel/"
+        const val YOUTUBE_MUSIC_MASK = "music.youtube.com/channel/"
+        const val AMAZON_MUSIC_MASK = "music.amazon.com/artists/"
+        const val TIKTOK_MASK = "tiktok.com/"
+        const val FACEBOOK_MASK = "facebook.com/"
+        const val THREADS_MASK = "threads.net/"
+        const val TWITTER_MASK = "twitter.com/"
     }
 }
